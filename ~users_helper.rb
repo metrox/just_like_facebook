159bfa7782eb62394110cf8905164a7793184717
metrox <dawiddyderski@gmail.com>
2011-07-11 17:10:26 +0200

    alfa_beta

--- ~users_helper.rb
+++ ~users_helper.rb
@@ -6,9 +6,14 @@ module UsersHelper
 
   def friend_finder(friendlist, id)
     friends_list = []
+
+<<<<<<< HEAD
     #usunac te w ktorych uzytkownik nie bierze udzialu
     friendlist.reject! {|friend| friend.id_user.to_i != id.to_i && 
 friend.id_friend.to_i != id.to_i  }    
+
+=======
+>>>>>>> 454cc998c5539ed6d2c06322bcd51d4db10c4b9b
     friendlist.each do |friend|
      # frieds list whithout us 
      (friend.id_user.to_i != id.to_i) ? (friends_list << friend.id_user) : (friends_list << friend.id_friend)
